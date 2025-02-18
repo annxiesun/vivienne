c418e390a4ac6ad5e91ca58df6eebaa2e803460d
Josephine Widjaja <j4widjaj@uwaterloo.ca>
2025-02-18 03:24:16 -0500

    home buttons cleanup

--- lib/components/button-component.tsx
+++ lib/components/button-component.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { ReactNode, useState } from "react";
 
-interface ButtonComponentProps { 
-    label: string;
-    children?: ReactNode;
+interface ButtonComponentProps {
+  label: string;
+  children?: ReactNode;
 }
 
-export default function ButtonComponent({ label, children = null }: ButtonComponentProps) {
+export default function ButtonComponent({
+  label,
+  children = null,
+}: ButtonComponentProps) {
   const [isVisible, setIsVisible] = useState(false);
 
   const handleClick = () => {
@@ -26,12 +31,12 @@ export default function ButtonComponent({ label, children = null }: ButtonCompon
       </button>
 
       {isVisible && (
-        <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-75 flex justify-center items-center z-50">
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-400 flex justify-center items-center z-50">
           <button
             onClick={handleClose}
-            className="absolute top-4 right-4 text-white text-2xl font-bold"
+            className="absolute top-2 right-2 text-gray-500 bg-gray-200 hover:bg-gray-300 transition p-1 rounded-md font-bold"
           >
-            &times;
+            Exit App
           </button>
           {children}
         </div>
