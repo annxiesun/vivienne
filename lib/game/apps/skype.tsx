943ac4aec759ca3fb5bc8d55ee6df8768bf47242
Josephine Widjaja <j4widjaj@uwaterloo.ca>
2025-02-25 18:48:00 -0500

    Lint

--- lib/game/apps/skype.tsx
+++ lib/game/apps/skype.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Video, Phone, PhoneOutgoing, PhoneOff } from "lucide-react";
+import { Video, Phone, PhoneOutgoing } from "lucide-react";
 
 export type Message = {
   text?: string;
