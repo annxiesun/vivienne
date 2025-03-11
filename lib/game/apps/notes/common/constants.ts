e5a68a1633f7166d3797699344cfa7b0d1787a68
Josephine Widjaja <j4widjaj@uwaterloo.ca>
2025-03-10 22:17:52 -0400

    write note 1

--- lib/game/apps/notes/common/constants.ts
+++ lib/game/apps/notes/common/constants.ts
@@ -10,14 +10,15 @@ export type Note = {
 
 export const initialNotes: Note[] = [
   {
-    title: "Public Note 1",
-    content: "This is a public note.",
-    date: "12/27/2025",
+    title: "diary - 3/2/2016",
+    content:
+      "being an influencer on instagram is so much harder than people think. lately, it feels like everything is falling apart. i have this pressure to constantly be 'on' — posting, sharing, being happy, and perfect for everyone. but it’s so exhausting. behind the filtered photos and perfectly curated posts, there’s a lot of emotional weight. i feel like i’m going through a lot right now, but i can’t let anyone see the real me. i just wish i could have a moment to breathe and not worry about what others think...",
+    date: "3/2/2016",
     hidden: false,
     folder: "public",
   },
   {
-    title: "Public Note 2",
+    title: "diary - 3/5/2016",
     content: "Another public note.",
     date: "12/28/2025",
     hidden: false,
