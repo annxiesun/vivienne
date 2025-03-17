842085a21c11504c7af91dba1741a8bbe61df698
Josephine Widjaja <j4widjaj@uwaterloo.ca>
2025-03-16 22:36:06 -0400

    Implement Skype story and progression

--- lib/game/apps/skype/components/image.tsx
+++ lib/game/apps/skype/components/image.tsx
@@ -14,10 +14,10 @@ const ImageModal = () => {
       className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50"
       onClick={() => toggleImageModal(false)}
     >
-      <div className="relative">
+      <div className="flex justify-center items-center">
         <img
           src={modalMessage}
-          className="max-w-[90%] max-h-[90%] rounded-lg shadow-lg"
+          className="max-w-[50%] max-h-[50%] rounded-lg shadow-lg object-contain"
         />
       </div>
     </div>
