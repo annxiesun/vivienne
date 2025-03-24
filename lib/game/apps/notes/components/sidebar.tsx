6b1659680b699c02b24be68f78d6eb6d3feed074
Josephine Widjaja <j4widjaj@uwaterloo.ca>
2025-03-24 16:29:32 -0400

    lint

--- lib/game/apps/notes/components/sidebar.tsx
+++ lib/game/apps/notes/components/sidebar.tsx
@@ -1,10 +1,7 @@
-import { useGameActions } from "../../../../state/context";
 import PrivateNotes from "./private";
 import PublicNotes from "./public";
 
 const Sidebar = () => {
-  const actions = useGameActions();
-
   return (
     <div className="w-1/4 h-full shadow-lg bg-white border-r border-gray-300 flex flex-col rounded-lg">
       <h2 className="text-2xl font-semibold px-6 py-4 text-gray-700">Notes</h2>
